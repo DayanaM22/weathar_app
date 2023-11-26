3a5215a6995ba66523f08317d055334534bdef51
DayanaM22 <148302813+DayanaM22@users.noreply.github.com>
2023-11-25 20:50:28 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+import requests
+from dotenv import load_dotenv,dotenv_values
+
+
+config = dotenv_values('.env')
+
+
+app = Flask(__name__)
+
+def get_weather_data(city):
+    API_KEY = config['API_KEY']
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&lang=es&units=metric'
+    r = requests.get(url).json()
+    print(r)
+    return r
+
+@app.route("/prueba")
+def prueba(): 
+    clima= get_weather_data("Ambato")
+    temperatura= str(clima['main']['temp'])
+    descripcion= str(clima['weather'][0]['description'])
+    icono= str(clima['weather'][0]['icon'])
+    
+    r_json = {
+        'ciudad':'Ambato',
+        'temperatura':temperatura,
+        'descripcion':descripcion,
+        'icono':icono
+        }
+    return render_template('weather.html',clima = r_json)
+
+if __name__ == '__main__':
+    app.run(debug=True) 
+    
